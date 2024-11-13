852e9dc9a70e5cbdcba42596b29155609c59911e
Vi Le <lecongvi18052002@gmail.com>
2024-11-13 13:39:01 +0700

    Initialize top interview 150

--- binary-search/_MEDIUM_2563_BinarySearch.cpp
+++ binary-search/_MEDIUM_2563_BinarySearch.cpp
@@ -4,23 +4,22 @@ using namespace std;
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        sort(nums.begin(), nums.end());
-        long long ans = 0;
-        for(int i=0; i<nums.size(); i++){
-            int low = lower_bound(nums, i+1, nums.size()-1, lower - nums[i]);
-            int high = lower_bound(nums, i+1, nums.size()-1, upper - nums[i]+1);
-            ans += 1LL * (high-low);
-        }
-        return ans;
+		sort(nums.begin(), nums.end());
+		long long ans = 0;
+		for(int i=0; i<nums.size(); i++){
+			int low = lower_bound(nums, i+1, nums.size()-1, lower-nums[i]);
+			int high = lower_bound(nums, i+1, nums.size()-1, upper-nums[i]+1);
+			ans += 1LL * (high-low);
+		}
+		return ans;
     }
-
     long long lower_bound(vector<int>& nums, int low, int high, int target){
-        while(low <= high){
-            int mid = low + ((high-low) / 2);
-            if(nums[mid] >= target) high = mid-1;
-            else low = mid+1;
-        }
-        return low;
+    	while(low <= high){
+    		int mid = low+((high-low) / 2);
+    		if(nums[mid] >= target) high=mid-1;
+    		else low=mid+1;
+    	}
+    	return low;
     }
 };
 
