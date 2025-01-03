df993fb37e9235fe1f514cff85b4674f6dfb10e0
Vi Le <lecongvi18052002@gmail.com>
2025-01-03 11:41:29 +0700

    Solved 2270 Number of Ways to Split Array with Brute Force

---
+++ 01 badge 2025/03_2270. Number of Ways to Split Array/solution1-bruteforce.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        long left=0, right=0;
+        int res=0;
+        for(int i=0; i<nums.size(); i++) right += nums[i];
+        for(int i=0; i<nums.size()-1; i++){
+        	left += nums[i];
+        	right -= nums[i];
+        	if(left>=right) res++;
+        }
+        return res;
+    }
+};
+
+int main(){
+	Solution sol;
+	vector<int> nums = {10,4,-8,7};
+	cout << sol.waysToSplitArray(nums) << endl;
+	return 0;
+}
